00c2034ff49d985cc1a4ebae9c23b05af2f5bd78
Yosuke Onoue <onoue@likr-lab.com>
2021-09-26 10:37:22 +0900

    Updates

---
+++ static/js/54.af7d0c62.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpundefined=this.webpackJsonpundefined||[]).push([[54],{102:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var i=e(10),o=e(4),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=54.af7d0c62.chunk.js.map
